6860f2767542212d33fbf6ee566faa50a272cccf
bretten <brettinbox@gmail.com>
2015-05-31 20:34:13 -0700

    Moved API methods ctag, status, report, capsule, discovery from ApiController to ApiComponent

--- app/Controller/ApiController.php
+++ app/Controller/ApiController.php
@@ -1,5 +1,6 @@
 <?php
 App::uses('AppController', 'Controller');
+
 /**
  * API Controller
  *
@@ -74,36 +75,7 @@ class ApiController extends AppController {
  * @return void
  */
     public function ctag($collection = null) {
-        $body = array();
-
-        if ($this->request->is('get')) {
-            // Will hold the ctag
-            $ctag;
-            // The collection
-            switch ($collection) {
-                case "capsules":
-                    $ctag = $this->Capsule->User->field('ctag_capsules', array('User.id' => $this->StatelessAuth->user('id')));
-                    break;
-                case "discoveries":
-                    $ctag = $this->Discovery->User->field('ctag_discoveries', array('User.id' => $this->StatelessAuth->user('id')));
-                    break;
-                default:
-                    $ctag = '';
-                    $this->response->statusCode(404);
-                    break;
-            }
-            // Build the response body
-            if ($ctag) {
-                $body['data'] = array(
-                    'ctag' => $ctag
-                );
-                $this->response->statusCode(200);
-            }
-        } else {
-            $this->response->statusCode(405);
-        }
-
-        $this->response->body(json_encode($body));
+        $this->Api->ctag($collection);
     }
 
 /**
@@ -113,57 +85,7 @@ class ApiController extends AppController {
  * @return void
  */
     public function status($collection = null) {
-        $body = array();
-
-        if ($this->request->is('get')) {
-            // The collection
-            switch ($collection) {
-                case "capsules":
-                    $capsules = $this->Capsule->find('all', array(
-                        'conditions' => array(
-                            'Capsule.user_id' => $this->StatelessAuth->user('id')
-                        ),
-                        'fields' => array('Capsule.id', 'Capsule.etag')
-                    ));
-                    $body = Hash::map($capsules, "{n}.Capsule", function($data) {
-                        return array(
-                            'data' => array(
-                                'id' => $data['id'],
-                                'etag' => $data['etag']
-                            )
-                        );
-                    });
-                    break;
-                case "discoveries":
-                    $discoveries = $this->Discovery->find('all', array(
-                        'conditions' => array(
-                            'Discovery.user_id' => $this->StatelessAuth->user('id')
-                        ),
-                        'fields' => array('Discovery.capsule_id', 'Discovery.etag')
-                    ));
-                    $body = Hash::map($discoveries, "{n}.Discovery", function($data) {
-                        return array(
-                            'data' => array(
-                                'id' => $data['capsule_id'],
-                                'etag' => $data['etag']
-                            )
-                        );
-                    });
-                    break;
-                default:
-                    $this->response->statusCode(404);
-                    break;
-            }
-        } else {
-            $this->response->statusCode(405);
-        }
-
-        // Indicate success if the response body was built
-        if ($body) {
-            $this->response->statusCode(200);
-        }
-
-        $this->response->body(json_encode($body));
+        $this->Api->status($collection);
     }
 
 /**
@@ -173,62 +95,7 @@ class ApiController extends AppController {
  * @return void
  */
     public function report($collection = null) {
-        $body = array();
-
-        if ($this->request->is('post')) {
-            if (!isset($this->request->data['id']) || !is_array($this->request->data['id'])) {
-                $this->response->statusCode(400);
-            } else {
-                // The collection
-                switch ($collection) {
-                    case "capsules":
-                        $capsules = $this->Capsule->find('all', array(
-                            'conditions' => array(
-                                'Capsule.id' => $this->request->data['id'],
-                                'Capsule.user_id' => $this->StatelessAuth->user('id')
-                            ),
-                            'fields' => array('Capsule.id', 'Capsule.name', 'Capsule.lat', 'Capsule.lng', 'Capsule.etag')
-                        ));
-                        $body = Hash::map($capsules, "{n}.Capsule", function($data) {
-                            return array(
-                                'data' => $data
-                            );
-                        });
-                        break;
-                    case "discoveries":
-                        $discoveries = $this->Discovery->find('all', array(
-                            'conditions' => array(
-                                'Discovery.capsule_id' => $this->request->data['id'],
-                                'Discovery.user_id' => $this->StatelessAuth->user('id')
-                            ),
-                            'fields' => array('Discovery.capsule_id', 'Discovery.favorite', 'Discovery.rating', 'Discovery.etag')
-                        ));
-                        $body = Hash::map($discoveries, "{n}.Discovery", function($data) {
-                            return array(
-                                'data' => array(
-                                    'id' => $data['capsule_id'],
-                                    'favorite' => $data['favorite'],
-                                    'rating' => $data['rating'],
-                                    'etag' => $data['etag']
-                                )
-                            );
-                        });
-                        break;
-                    default:
-                        $this->response->statusCode(404);
-                        break;
-                }
-            }
-        } else {
-            $this->response->statusCode(405);
-        }
-
-        // Indicate success if the response body was built
-        if ($body) {
-            $this->response->statusCode(200);
-        }
-
-        $this->response->body(json_encode($body));
+        $this->Api->report($collection);
     }
 
 /**
@@ -255,62 +122,7 @@ class ApiController extends AppController {
  * @return void
  */
     public function capsule($id = null) {
-        $body = array();
-
-        if ($this->request->is('post')) {
-            $this->request->data['user_id'] = $this->StatelessAuth->user('id');
-            if ($result = $this->Capsule->saveDiff($this->request->data, array(
-                'deep' => true, 'removeHasMany' => 'Memoir', 'updateCtagForUser' => $this->StatelessAuth->user('id')
-            ))) {
-                // Determine the Capsule id
-                $updateId = 0;
-                if (isset($this->request->data['id'])) { // UPDATE
-                    $updateId = $this->request->data['id'];
-                } else { // INSERT
-                    $updateId = $this->Capsule->getLastInsertId();
-                }
-                // Get the new Capsule data
-                $capsule = $this->Capsule->find('first', array(
-                    'conditions' => array(
-                        'Capsule.id' => $updateId,
-                        'Capsule.user_id' => $this->StatelessAuth->user('id')
-                    ),
-                    'fields' => array('Capsule.id', 'Capsule.name', 'Capsule.lat', 'Capsule.lng', 'Capsule.etag')
-                ));
-                if ($capsule && isset($capsule['Capsule'])) {
-                    $body['data'] = $capsule['Capsule'];
-                } else {
-                    $this->response->statusCode(500);
-                }
-            } else {
-                $this->response->statusCode(500);
-            }
-        } elseif ($this->request->is('get')) {
-            $this->response->statusCode(501);
-        } elseif ($this->request->is('delete')) {
-            $this->Capsule->id = $id;
-            if (!$this->Capsule->exists()) {
-                $this->response->statusCode(404);
-            } elseif (!$this->Capsule->ownedBy($this->StatelessAuth->user('id'))) {
-                $this->response->statusCode(405);
-            } else {
-                if ($this->Capsule->delete()) {
-                    $this->response->statusCode(204);
-                    return;
-                } else {
-                    $this->response->statusCode(500);
-                }
-            }
-        } else {
-            $this->response->statusCode(405);
-        }
-
-        // Indicate success if the response body was built
-        if ($body) {
-            $this->response->statusCode(200);
-        }
-
-        $this->response->body(json_encode($body));
+        $this->Api->capsule($id);
     }
 
 /**
@@ -319,40 +131,7 @@ class ApiController extends AppController {
  * @return void
  */
     public function discovery($id = null) {
-        $body = array();
-
-        if ($this->request->is('post')) {
-            if (!isset($this->request->data['id'])) {
-                $this->response->statusCode(400);
-            } elseif (!$exists = $this->Capsule->Discovery->created(
-                    $this->request->data['id'],
-                    $this->StatelessAuth->user('id')
-            )) {
-                $this->response->statusCode(404);
-            } else {
-                $this->request->data['id'] = $exists['Discovery']['id'];
-                if ($result = $this->Discovery->save($this->request->data, array('updateCtagForUser' => $this->StatelessAuth->user('id')))) {
-                    $body['data'] = array(
-                        'etag' => $result['Discovery']['etag']
-                    );
-                } else {
-                    $this->response->statusCode(500);
-                }
-            }
-        } elseif ($this->request->is('get')) {
-            $this->response->statusCode(501);
-        } elseif ($this->request->is('delete')) {
-            $this->response->statusCode(405);
-        } else {
-            $this->response->statusCode(405);
-        }
-
-        // Indicate success if the response body was built
-        if ($body) {
-            $this->response->statusCode(200);
-        }
-
-        $this->response->body(json_encode($body));
+        $this->Api->discovery($id);
     }
 
 }
--- app/Controller/Component/ApiComponent.php
+++ app/Controller/Component/ApiComponent.php
@@ -264,7 +264,7 @@ class ApiComponent extends Component {
             Configure::read('Map.UserLocation.SearchRadius')
         );
         // Add them to the response
-        $this->body['data']['capsules'] = Hash::map($capsules, "{n}.Capsule", function($data) {
+        $this->body['data']['capsules'] = Hash::map($capsules, "{n}.Capsule", function ($data) {
             return ApiComponent::buildCapsule($data);
         });
 
@@ -343,13 +343,342 @@ class ApiComponent extends Component {
     }
 
 /**
+ * API method to get the ctag for the specified collection
+ *
+ * @param string $collection Collection to get the ctag for
+ * @return string
+ */
+    public function ctag($collection = null) {
+        // Make sure it is a GET request
+        if (!$this->request->is('get')) {
+            // Indicate that the request method is not allowed
+            return $this->responseNotAllowed();
+        }
+
+        // Make sure the User is authenticated
+        if (!$this->Auth->user()) {
+            // Indicate that the request was unauthenticated
+            return $this->responseUnauthenticated();
+        }
+
+        // Make sure the collection was specified
+        if (!$collection) {
+            // Indicate a bad request
+            return $this->responseBadRequest();
+        }
+
+        // Get the ctag based on which collection
+        $ctag = "";
+        switch ($collection) {
+            case "capsules":
+                $ctag = $this->controller->User->field('ctag_capsules', array('User.id' => $this->Auth->user('id')));
+                break;
+            case "discoveries":
+                $ctag = $this->controller->User->field('ctag_discoveries', array('User.id' => $this->Auth->user('id')));
+                break;
+            default:
+                // Unrecognized collection, so indicate a bad request
+                return $this->responseBadRequest();
+                break;
+        }
+
+        // Add the ctag to the response body
+        $this->body['data']['ctag'] = $ctag;
+
+        // Indicate success
+        return $this->responseOk();
+    }
+
+/**
+ * API method to retrieve the entity status on the specified collection
+ *
+ * @param string $collection The collection to get the entity status on
+ * @return string
+ */
+    public function status($collection = null) {
+        // Make sure it is a GET request
+        if (!$this->request->is('get')) {
+            // Indicate that the request method is not allowed
+            return $this->responseNotAllowed();
+        }
+
+        // Make sure the User is authenticated
+        if (!$this->Auth->user()) {
+            // Indicate that the request was unauthenticated
+            return $this->responseUnauthenticated();
+        }
+
+        // Make sure the collection was specified
+        if (!$collection) {
+            // Indicate a bad request
+            return $this->responseBadRequest();
+        }
+
+        // Determine which collection to get status on
+        switch ($collection) {
+            case "capsules":
+                $capsules = $this->controller->Capsule->find('all', array(
+                    'conditions' => array(
+                        'Capsule.user_id' => $this->Auth->user('id')
+                    ),
+                    'fields' => array('Capsule.id', 'Capsule.etag')
+                ));
+                // Add them to the response
+                $this->body['data']['capsules'] = Hash::map($capsules, "{n}.Capsule", function ($data) {
+                    return ApiComponent::buildCapsule($data);
+                });
+                break;
+            case "discoveries":
+                $discoveries = $this->controller->Capsule->find('all', array(
+                    'joins' => array(
+                        array(
+                            'table' => 'discoveries',
+                            'alias' => 'Discovery',
+                            'type' => 'INNER',
+                            'conditions' => array(
+                                'Capsule.id = Discovery.capsule_id',
+                                'Discovery.user_id' => $this->Auth->user('id')
+                            )
+                        )
+                    ),
+                    'fields' => array('Capsule.id', 'Discovery.etag')
+                ));
+                // Add them to the response
+                $this->body['data']['discoveries'] = Hash::map($discoveries, "{n}", function ($data) {
+                    return ApiComponent::buildDiscovery($data['Capsule'], $data['Discovery']);
+                });
+                break;
+            default:
+                // Unrecognized collection, so indicate a bad request
+                return $this->responseBadRequest();
+                break;
+        }
+
+        // Indicate success
+        return $this->responseOk();
+    }
+
+/**
+ * API method to report on the specified collection and ids
+ *
+ * @param string $collection The collection to report on
+ * @return string
+ */
+    public function report($collection = null) {
+        // Make sure it is a GET request
+        if (!$this->request->is('post')) {
+            // Indicate that the request method is not allowed
+            return $this->responseNotAllowed();
+        }
+
+        // Make sure the User is authenticated
+        if (!$this->Auth->user()) {
+            // Indicate that the request was unauthenticated
+            return $this->responseUnauthenticated();
+        }
+
+        // Make sure the collection was specified
+        if (!$collection || !isset($this->request->data['id'])) {
+            // Indicate a bad request
+            return $this->responseBadRequest();
+        }
+
+        // Determine which collection to report on
+        switch ($collection) {
+            case "capsules":
+                $capsules = $this->controller->Capsule->find('all', array(
+                    'conditions' => array(
+                        'Capsule.id' => $this->request->data['id'],
+                        'Capsule.user_id' => $this->Auth->user('id')
+                    ),
+                    'fields' => array('Capsule.id', 'Capsule.name', 'Capsule.lat', 'Capsule.lng', 'Capsule.etag')
+                ));
+                // Add them to the response
+                $this->body['data']['capsules'] = Hash::map($capsules, "{n}.Capsule", function ($data) {
+                    return ApiComponent::buildCapsule($data);
+                });
+                break;
+            case "discoveries":
+                $discoveries = $this->controller->Capsule->find('all', array(
+                    'conditions' => array(
+                        'Capsule.id' => $this->request->data['id'],
+                        'Discovery.user_id' => $this->Auth->user('id')
+                    ),
+                    'joins' => array(
+                        array(
+                            'table' => 'discoveries',
+                            'alias' => 'Discovery',
+                            'type' => 'INNER',
+                            'conditions' => array(
+                                'Capsule.id = Discovery.capsule_id',
+                                'Discovery.user_id' => $this->Auth->user('id')
+                            )
+                        )
+                    ),
+                    'fields' => array(
+                        'Capsule.id', 'Capsule.name', 'Capsule.lat', 'Capsule.lng',
+                        'Discovery.favorite', 'Discovery.rating', 'Discovery.etag'
+                    )
+                ));
+                // Add them to the response
+                $this->body['data']['discoveries'] = Hash::map($discoveries, "{n}", function ($data) {
+                    return ApiComponent::buildDiscovery($data['Capsule'], $data['Discovery']);
+                });
+                break;
+            default:
+                // Unrecognized collection, so indicate a bad request
+                return $this->responseBadRequest();
+                break;
+        }
+
+        // Indicate success
+        return $this->responseOk();
+    }
+
+/**
+ * API method to perform various actions on a single Capsule
+ *
+ * @param int $id
+ * @return string
+ */
+    public function capsule($id = null) {
+        // Make sure the User is authenticated
+        if (!$this->Auth->user()) {
+            // Indicate that the request was unauthenticated
+            return $this->responseUnauthenticated();
+        }
+
+        // Vary the action based on the HTTP method
+        if ($this->request->is('post')) {
+            // Build the data to save
+            $data = array(
+                'Capsule' => $this->request->data
+            );
+            if ($id) {
+                $data['Capsule']['id'] = $id;
+                // Make sure the Capsule exists and that it belongs to the User
+                if (!$this->controller->Capsule->exists($id)
+                    || !$this->controller->Capsule->ownedBy($this->Auth->user('id'), $id)) {
+                    // Indicate the Capsule was not found
+                    return $this->responseNotFound();
+                }
+            }
+            // INSERT or UPDATE a Capsule
+            if ($this->controller->Capsule->saveDiff($data, array(
+                'deep' => true, 'removeHasMany' => 'Memoir', 'associateOwner' => true,
+                'updateCtagForUser' => $this->Auth->user('id')
+            ))) {
+                // Determine its ID
+                $capsuleId = 0;
+                if (isset($id)) {
+                    $capsuleId = $id;
+                } else {
+                    $capsuleId = $this->controller->Capsule->getLastInsertId();
+                }
+
+                // Get the new Capsule data
+                $capsule = $this->controller->Capsule->find('first', array(
+                    'conditions' => array(
+                        'Capsule.id' => $capsuleId,
+                        'Capsule.user_id' => $this->Auth->user('id')
+                    ),
+                    'fields' => array('Capsule.id', 'Capsule.name', 'Capsule.lat', 'Capsule.lng', 'Capsule.etag')
+                ));
+                if ($capsule) {
+                    $this->body['data']['capsule'] = $capsule['Capsule'];
+                    // Indicate a success
+                    return $this->responseOk();
+                }
+            }
+
+            // If the POST request made it here, then something went wrong
+            return $this->responseServerError();
+        } else if ($this->request->is('delete')) {
+            // DELETE a Capsule
+            $this->controller->Capsule->id = $id;
+            // Make sure the Capsule exists and belongs to the User
+            if (!$this->controller->Capsule->exists() || !$this->controller->Capsule->ownedBy($this->Auth->user('id'))) {
+                // Indicate the Capsule was not found
+                return $this->responseNotFound();
+            }
+            // Delete
+            if ($this->controller->Capsule->delete()) {
+                // Indicate a success (no response body)
+                return $this->responseNoContent();
+            } else {
+                // Indicate a server error
+                return $this->responseServerError();
+            }
+        } else {
+            // The HTTP method is not allowed, so indicate that in the response
+            return $this->responseNotAllowed();
+        }
+    }
+
+/**
+ * API method to perform various actions on a single Discovery
+ *
+ * @param int $id
+ * @return string
+ */
+    public function discovery($id = null) {
+        // Make sure the User is authenticated
+        if (!$this->Auth->user()) {
+            // Indicate that the request was unauthenticated
+            return $this->responseUnauthenticated();
+        }
+
+        // Vary the action based on the HTTP method
+        if ($this->request->is('post')) {
+            // Make sure an ID is specified
+            if (!$id) {
+                // Indicate a bad request
+                return $this->responseBadRequest();
+            }
+            // Make sure the User has discovered this Capsule
+            if (!$exists = $this->controller->Discovery->created($id, $this->Auth->user('id'))) {
+                // Indicate the resource was not found
+                return $this->responseNotFound();
+            }
+            // UPDATE the Discovery
+            $this->request->data['id'] = $exists['Discovery']['id'];
+            if ($result = $this->controller->Discovery->save($this->request->data, array(
+                'updateCtagForUser' => $this->Auth->user('id')
+            ))) {
+                // Add the updated Discovery to the body
+                $this->body['data']['discovery'] = ApiComponent::buildDiscovery(array('id' => $id), $result['Discovery']);
+                // Indicate a success
+                return $this->responseOk();
+            }
+
+            // If the POST request made it here, then something went wrong
+            return $this->responseServerError();
+        } else {
+            // The HTTP method is not allowed, so indicate that in the response
+            return $this->responseNotAllowed();
+        }
+    }
+
+/**
  * Sets the status code to indicate a OK success and sets the body
  *
  * @return string
  */
     private function responseOk() {
-        // Indicate a bad request
+        // Indicate success
         $this->response->statusCode(200);
+        // Set the body
+        return $this->setBody();
+    }
+
+/**
+ * Sets the status code to indicate there is no body content
+ *
+ * @return string
+ */
+    private function responseNoContent() {
+        // Indicate that there is no content
+        $this->response->statusCode(204);
         // Set the body
         return $this->setBody();
     }
@@ -438,7 +767,7 @@ class ApiComponent extends Component {
 /**
  * Builds a Capsule data array to be sent over the wire
  *
- * @param $capsule Capsule data
+ * @param array $capsule Capsule data
  * @return array
  */
     public static function buildCapsule($capsule) {
@@ -464,8 +793,8 @@ class ApiComponent extends Component {
 /**
  * Builds a Discovery data array to be sent over the wire
  *
- * @param $capsule Capsule data
- * @param $discovery Discovery data
+ * @param array $capsule Capsule data
+ * @param array $discovery Discovery data
  * @return array
  */
     public static function buildDiscovery($capsule, $discovery) {
