e6c3d6fd0c9396eabc5f67b9f4f701100bbd8483
bretten <brettinbox@gmail.com>
2015-06-17 03:07:46 -0700

    Added fields to store information about uploads to the Memoir table and added corresponding validation

--- app/Model/Memoir.php
+++ app/Model/Memoir.php
@@ -1,5 +1,6 @@
 <?php
 App::uses('AppModel', 'Model');
+
 /**
  * Memoir Model
  *
@@ -35,7 +36,7 @@ class Memoir extends AppModel {
                 'required' => true
             ),
             'maxLength' => array(
-                'rule' => array('maxLength' ,255),
+                'rule' => array('maxLength', 255),
                 'message' => 'The title cannot exceed 255 characters.'
             )
         ),
@@ -44,6 +45,49 @@ class Memoir extends AppModel {
                 'rule' => 'notEmpty',
                 'message' => 'Please choose a file.',
                 'required' => true
+            )
+        ),
+        'file_location' => array(
+            'notEmpty' => array(
+                'rule' => 'notEmpty',
+                'message' => 'Please enter a file location.',
+                'required' => true
+            )
+        ),
+        'file_public_name' => array(
+            'notEmpty' => array(
+                'rule' => 'notEmpty',
+                'message' => 'Please enter a file name.',
+                'required' => true
+            )
+        ),
+        'file_original_name' => array(
+            'notEmpty' => array(
+                'rule' => 'notEmpty',
+                'message' => 'Please enter a file name.',
+                'required' => true
+            )
+        ),
+        'file_type' => array(
+            'notEmpty' => array(
+                'rule' => 'notEmpty',
+                'message' => 'Please enter a file type.',
+                'required' => true
+            ),
+            'maxLength' => array(
+                'rule' => array('maxLength', 64),
+                'message' => 'The title cannot exceed 64 characters.'
+            )
+        ),
+        'file_size' => array(
+            'notEmpty' => array(
+                'rule' => 'notEmpty',
+                'message' => 'Please enter a file size.',
+                'required' => true
+            ),
+            'numeric' => array(
+                'rule' => 'numeric',
+                'message' => 'Please enter a valid file size.'
             )
         ),
         'order' => array(
