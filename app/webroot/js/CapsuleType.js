97576117f9e8ab88408bbe5ef9677241f2ae42e9
bretten <brettinbox@gmail.com>
2015-11-19 15:32:35 -0800

    Added Javascript object that defines the types of Capsules

---
+++ app/webroot/js/CapsuleType.js
@@ -0,0 +1,10 @@
+/**
+ * The object "namespace" that defines the different types of Capsules
+ *
+ * @type {{Capsule: string, Discovery: string}}
+ * @author https://github.com/bretten
+ */
+var CapsuleType = {
+    Capsule: "Capsule",
+    Discovery: "Discovery"
+};
