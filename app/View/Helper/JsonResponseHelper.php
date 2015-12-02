de0d767293fa5f31e5615189a32f4bd0e17b90d1
bretten <brettinbox@gmail.com>
2015-12-02 02:35:46 -0800

    JsonResponseHelper now removes unnecessary Capsule data

--- app/View/Helper/JsonResponseHelper.php
+++ app/View/Helper/JsonResponseHelper.php
@@ -74,6 +74,9 @@ class JsonResponseHelper extends AppHelper {
      * @param array $capsule The Capsule data
      */
     public function addCapsuleToData(array $capsule) {
+        // Remove any unnecessary Capsule data
+        $capsule = $this->removeUnnecessaryCapsuleData($capsule);
+
         $this->data['capsule'] = $capsule;
     }
 
@@ -83,6 +86,10 @@ class JsonResponseHelper extends AppHelper {
      * @param array $capsules Collection of Capsule data
      */
     public function addCapsulesToData(array $capsules) {
+        // Remove any unnecessary Capsule data
+        $capsules = $this->removeUnnecessaryCapsuleData($capsules, /* isCollection */
+            true);
+
         $this->data['capsules'] = $capsules;
     }
 
@@ -102,6 +109,29 @@ class JsonResponseHelper extends AppHelper {
      */
     public function addCtagToData($ctag = "") {
         $this->data['ctag'] = $ctag;
+    }
+
+    /**
+     * Removes unnecessary Capsule-related data from the specified data array
+     *
+     * @param array $data The array to remove unnecessary data from
+     * @param bool|false $isCollection Whether or not the data array contains a collection of Capsules
+     * @return array The modified data array
+     */
+    private function removeUnnecessaryCapsuleData(array $data, $isCollection = false) {
+        // Determine the path prefixes based on whether or not the data is a collection
+        $capsulePathPrefix = $isCollection ? "{n}.Capsule" : "Capsule";
+        $memoirPathPrefix = $isCollection ? "{n}.Memoir" : "Memoir.{n}";
+        // Remove the User ID
+        $data = Hash::remove($data, $capsulePathPrefix . ".user_id");
+        // Remove Memoir information
+        $data = Hash::remove($data, $memoirPathPrefix . ".file_location");
+        $data = Hash::remove($data, $memoirPathPrefix . ".file_public_name");
+        $data = Hash::remove($data, $memoirPathPrefix . ".file_type");
+        $data = Hash::remove($data, $memoirPathPrefix . ".file_size");
+        $data = Hash::remove($data, $memoirPathPrefix . ".modified");
+
+        return $data;
     }
 
 }
